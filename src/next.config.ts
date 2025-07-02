dd6b2991fb89563455a205d921b418c12359b8c7
File Vault <datavault5546@gmail.com>
2025-07-02 05:39:34 +0000

    Failed to load resource: the server responded with a status of 413 () 16

---
+++ src/next.config.ts
@@ -0,0 +1,27 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  serverActions: {
+    bodySizeLimit: '20mb',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
